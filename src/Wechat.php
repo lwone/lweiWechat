814d74461e119406dfffb7bf6a024bc7985bd4ca
lwei <1750433663@qq.com>
2021-05-25 15:16:36 +0800

    更新

--- src/Wechat.php
+++ src/Wechat.php
@@ -1,9 +1,36 @@
 <?php
 namespace lweiWechat;
 
+/**
+ * Class WechatService
+ * @package lweiWechat
+ * @method static \lweiWechat\service\OfficialAccounts               wechat(array $config = []) 微信公众号
+ */
 class Wechat
 {
-    public function hello(){
-        return "欢迎使用lwei-Wechat！";
+    /**
+     * @param $name
+     * @param array $config
+     * @return mixed
+     */
+    public static function make($name, array $config = [])
+    {
+        $name        = str_replace('wechat', '', $name);
+        $namespace   = 'Wechat' . ucfirst($name);
+        $application = "\\lweiWechat\\service\\{$namespace}";
+        return new $application($config);
+    }
+
+    /**
+     * Dynamically pass methods to the application.
+     *
+     * @param string $name
+     * @param array $arguments
+     *
+     * @return mixed
+     */
+    public static function __callStatic($name, $arguments)
+    {
+        return self::make($name, ...$arguments);
     }
 }
---
+++ src/service/OfficialAccounts.php
@@ -0,0 +1,19 @@
+<?php
+namespace lweiWechat\service;
+
+use EasyWeChat\Factory;
+
+/**
+ * 公众号类库
+ * Class OfficialAccounts
+ * @package lweiWechat\service
+ */
+class OfficialAccounts
+{
+
+
+    public function __construct($config)
+    {
+        $this->wechat = Factory::officialAccount($config);
+    }
+}
